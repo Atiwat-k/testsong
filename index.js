d403ee8fb4f59a01e417360001b12bb5ed6f4456
Atiwat <atiwat.kon@gmail.com>
2025-08-25 03:10:38 +0700

    s

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+import { createServer } from 'http';
 import express from 'express';
-import songRoutes from './songRoutes.js';
+import songRoutes from '../songRoutes.js';
 
 const app = express();
-
-// ให้มือถือเข้าถึง API ได้
 app.use(express.json());
 app.use('/songRoutes', songRoutes);
 
-const PORT = 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
