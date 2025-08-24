0fb4476f7d97f34d707cc2d72fb17ace67e94381
Atiwat <atiwat.kon@gmail.com>
2025-08-25 03:12:38 +0700

    หห

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import { createServer } from 'http';
 import express from 'express';
 import songRoutes from '../songRoutes.js';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
 const app = express();
 app.use(express.json());
 app.use('/songRoutes', songRoutes);
 
-export default app;
+// Vercel ต้อง export default function
+export default function handler(req, res) {
+  app(req, res);
+}
